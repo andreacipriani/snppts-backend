fab4c764b10ccb30ccadf812ba2cd1c636da2824
Andrea Cipriani <andrea.cipriani@soundcloud.com>
2017-02-10 17:43:36 +0100

    Fixed group_id generation for the first entity

--- snppts-backend/src/main/scala/com/snppts/backend/mongodb/GroupMongoController.scala
+++ snppts-backend/src/main/scala/com/snppts/backend/mongodb/GroupMongoController.scala
@@ -5,6 +5,8 @@ import com.mongodb.casbah.Imports._
 
 class GroupMongoController(mongoCollection: MongoCollection) extends ScalatraServlet {
 
+val groupId = "group_id"
+
   /**
    * Insert a new group into the database.
    * curl -i -H "Accept: application/json" -X POST -d "name=Ale" http://localhost:8080/api/v1/group
@@ -13,17 +15,21 @@ class GroupMongoController(mongoCollection: MongoCollection) extends ScalatraSer
   post("/") {
     val name = params("name")
     val maxGroupId = mongoMaxGroupId() + 1
-    val group = MongoDBObject("group_id" -> maxGroupId, "name" -> name)
+    val group = MongoDBObject(groupId -> maxGroupId, "name" -> name)
     mongoCollection += group
   }
 
   def mongoMaxGroupId() : Int = {
-    val fields = MongoDBObject("group_id" -> 1)
+    val fields = MongoDBObject(groupId -> 1)
     val orderByAsc = -1
-    val orderBy = MongoDBObject("group_id" -> orderByAsc)
+    val orderBy = MongoDBObject(groupId -> orderByAsc)
     val result = mongoCollection.findOne(fields = fields, orderBy = orderBy)
-    val group = result.get.getAs[Int]("group_id").get
-    return group
+    val firstId = 0
+
+    if (result.isDefined) {
+      return result.get.getAs[Int](groupId).get
+    }
+    return firstId
   }
 
   /**
@@ -41,8 +47,7 @@ class GroupMongoController(mongoCollection: MongoCollection) extends ScalatraSer
    */
 
   get("/query/:group_id") {
-    val groupId = params("group_id")
-    val query = MongoDBObject("group_id" -> groupId)
+    val query = MongoDBObject(groupId -> params(groupId))
     for ( x <- mongoCollection.findOne(query) ) yield x
   }
 
